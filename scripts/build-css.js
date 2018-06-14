1bbcce2a0486f283d0dedff3fd73d5fbf84c6ea2
Ivan Reyes <ivan.reyes@wizeline.com>
2018-06-14 16:11:27 -0500

    Import the rigth css file

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -7,7 +7,7 @@ const writeFileAsync = util.promisify(fs.writeFile)
 
 const source = [
   'node_modules/@natgeo/styleguide/lib/styles/styleguide-default.css',
-  'node_modules/@natgeo/modules-global-footer/lib/skinny/GlobalFooter.css',
+  'node_modules/@natgeo/modules-global-footer/lib/GlobalFooter.css',
   'node_modules/@natgeo/packages-registration/legacy/v5/main.scss',
   'node_modules/@natgeo/modules-global-nav/lib/GlobalNav.css',
   'node_modules/@natgeo/web-components-react-image/lib/styles.css',
