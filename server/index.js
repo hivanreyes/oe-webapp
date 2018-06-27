508be271fc878f79fd5777cb5b480ee7594adae5
Hermes <hermes.espinola@wizeline.com>
2018-06-27 16:48:06 -0500

    Add eslint style

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 const port = process.env.PORT || 3000;
 
-/* eslint no-console: 0*/
+/* eslint no-console: 0 */
 app.prepare()
   .then(() => {
     const server = express()
