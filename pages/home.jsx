78a64193ba23c1304a9b85dd121fca36efe8a47e
Hermes <hermes.espinola@wizeline.com>
2018-07-04 12:37:53 -0500

    Add ExpeditionFilterComponent (not working for some reason)

--- pages/home.jsx
+++ pages/home.jsx
@@ -4,8 +4,6 @@ import { Container } from 'semantic-ui-react'
 import Layout from '../components/shared/Layout'
 import Carousel from '../components/home/carousel'
 import Header from '../components/shared/Header'
-import Filter from '../components/shared/Filter'
-import ObservationCard from '../components/shared/ObservationCard'
 
 const { publicRuntimeConfig } = getConfig()
 
@@ -17,18 +15,6 @@ class Home extends Component {
         <Container className="image-container">
           <Carousel />
         </Container>
-        <Filter title={'Sopotamadre'}/>
-        <ObservationCard
-          data={{
-            name: 'MPA Watch',
-            media:
-              'https://s3-us-west-2.amazonaws.com/openexplorer-media-assets/images/f63576f8-e537-4d1c-8af7-b6fc3c9a714b-large.jpg?v=x',
-            shareText:
-              'Thrity oinfweoin weoinfweonfiowenfoiewnfioewnfoiwenfwieo noiwnfwoienfiowenfwoinfowenoifnaeoifnoaiwenfoiwnfoiwn fioweanfoiwenfoiwenafiowanfiownfoiawnfionwionweiofnweoifnwioa',
-            commentsCount: 10,
-            locationName: 'Sausalito, California',
-          }}
-        />
       </Layout>
     )
   }
