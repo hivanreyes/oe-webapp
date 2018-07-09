023f63d141478d2525e922f5553d2e50f9bc632a
Ivan Reyes <ivan.reyes@wizeline.com>
2018-07-08 19:43:38 -0500

    Refactor to slider carousel

--- components/shared/index.js
+++ components/shared/index.js
@@ -1,7 +1,9 @@
 import ExpeditionCard from './ExpeditionCard'
 import ExpeditionFilter from './ExpeditionFilter'
+import CarouselWrapper from './carouselWrapper'
 
 export {
   ExpeditionCard,
   ExpeditionFilter,
+  CarouselWrapper,
 }
